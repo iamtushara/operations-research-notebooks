58456039b7b17eb0bb513307fa1dd57e1c7dd270
Tushar Anand <tushar.anand@galytix.com>
2022-05-12 20:43:29 +0530

    first commit

---
+++ notebooks/project.py
@@ -0,0 +1,3 @@
+
+S3_PROJECT_URL='s3://galytix-labs/SRCH01/operations-research-notebooks'
+ML_FLOW_EXPT_NAME='operations-research-notebooks'
---
+++ notebooks/xx-python-notebook-template.py
@@ -0,0 +1,49 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.4.0
+#   kernelspec:
+#     display_name: Python [conda env:.conda-operations-research-notebooks]
+#     language: python
+#     name: conda-env-.conda-operations-research-notebooks-py
+# ---
+
+# %% [markdown]
+# # Introduction
+#
+# * todo
+
+# %% [markdown]
+# # Setup
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+import pandas as pd
+import numpy as np
+
+import project
+
+pd.set_option("display.max_colwidth",100)
+pd.set_option("display.max_rows",1000)
+
+# %% [markdown]
+#
+# # Data
+
+# %%
+#df = pd.read_csv(f"{project.S3_PROJECT_URL}/a_file.csv")
+#df.head()
+
+
+
+# %% [markdown]
+# # Content
+
+# %%
